5ecaceaaf5a32d613db3160a6063766d4dbd940f
Aaron Clayton <aaron.claytonj@outlook.com>
2023-04-22 15:18:42 -0500

    corrected some math when building grid, dont need width and hight

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const container = document.querySelector(".container");
 const sizeOfPage = getComputedStyle(container).maxWidth;
 let numPerRow = 16;
 
-for (let i = 1; i < numPerRow * numPerRow; i++) {
+let total = numPerRow * numPerRow + numPerRow;
+
+for (let i = 1; i < total; i++) {
   let div = document.createElement("div");
   if (i % (numPerRow + 1) == 0) {
     div.style.cssText = "border: 0; height: 0; width: 100%";
@@ -13,7 +15,7 @@ for (let i = 1; i < numPerRow * numPerRow; i++) {
     event.target.style.backgroundColor = "red";
     setTimeout(() => {
       event.target.style.backgroundColor = "";
-    }, 200);
+    }, 500);
   });
 
   container.appendChild(div);
