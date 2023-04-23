0e3dcc2c5d090259507b65cbf1a732846b21976d
Aaron Clayton <aaron.claytonj@outlook.com>
2023-04-22 21:20:09 -0500

    added button to chose grid size
    added event listener to reset the board and create new grid of size n

--- app.js
+++ app.js
@@ -1,22 +1,39 @@
-const container = document.querySelector(".container");
-const sizeOfPage = getComputedStyle(container).maxWidth;
-let numPerRow = 16;
+const gridSizeBtn = document.querySelector(".gridSizeBtn");
 
-let total = numPerRow * numPerRow + numPerRow;
-
-for (let i = 1; i < total; i++) {
-  let div = document.createElement("div");
-  if (i % (numPerRow + 1) == 0) {
-    div.style.cssText = "border: 0; height: 0; width: 100%";
-  } else {
-    div.style.cssText = `border: 1px solid black; height: calc((${sizeOfPage} /${numPerRow}) - 2px); width: calc((${sizeOfPage} /${numPerRow}) - 2px);`;
+gridSizeBtn.addEventListener("click", function (event) {
+  let choice = parseInt(prompt("Choose grid size! (max 80)"));
+  if (choice > 80) {
+    choice = 80;
   }
-  div.addEventListener("mouseenter", function (event) {
-    event.target.style.backgroundColor = "red";
-    setTimeout(() => {
-      event.target.style.backgroundColor = "";
-    }, 500);
-  });
+  const container = document.querySelector(".container");
+  container.remove();
 
-  container.appendChild(div);
+  gridSetup(choice);
+});
+
+function gridSetup(numPerRow) {
+  let container = document.createElement("div");
+  container.classList.add("container");
+  document.body.appendChild(container);
+  const sizeOfPage = getComputedStyle(container).maxWidth;
+
+  let total = numPerRow * numPerRow + numPerRow;
+  for (let i = 1; i < total; i++) {
+    let div = document.createElement("div");
+    if (i % (numPerRow + 1) == 0) {
+      div.style.cssText = "border: 0; height: 0; width: 100%";
+    } else {
+      div.style.cssText = `border: 1px solid black; height: calc((${sizeOfPage} /${numPerRow}) - 2px); width: calc((${sizeOfPage} /${numPerRow}) - 2px);`;
+    }
+    div.addEventListener("mouseenter", function (event) {
+      event.target.style.backgroundColor = "red";
+      // setTimeout(() => {
+      //   event.target.style.backgroundColor = "";
+      // }, 500);
+    });
+
+    container.appendChild(div);
+  }
 }
+
+gridSetup(16);
